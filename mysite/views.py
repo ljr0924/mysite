44d6bc345e952912e2f9f73a26cea68e14f7ed01
Ljr <13630461363@163.com>
2019-01-21 22:44:48 +0800

    修改文件

--- mysite/views.py
+++ mysite/views.py
@@ -7,7 +7,6 @@ def home(request):
     blog_content_type = ContentType.objects.get_for_model(Blog)
     date_list, read_nums_list = get_seven_days_read_data(blog_content_type)
 
-    print(read_nums_list)
     context = {}
     context['read_nums_list'] = read_nums_list
     context['date_list'] = date_list
