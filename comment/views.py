62f03978193f09c9ff0e2bbe04ff22d4f4ac443d
ljr <13630461363@163.com>
2019-01-23 17:58:28 +0800

    完整用户登录，用户评论逻辑

--- comment/views.py
+++ comment/views.py
@@ -12,14 +12,14 @@ def update_comment(request):
     # model_class()获得具体的模型对象
     model_class = ContentType.objects.get(model=content_type).model_class()
     model_obj = model_class.objects.get(pk=object_id)
-
+    # 记录评论内容，保存到数据库
     comment = Comment()
     comment.user = user
     comment.text = text
     comment.content_object = model_obj
     print(comment)
     comment.save()
-
+    # 重定向回原页面
     referer = request.META.get('HTTP_REFERER', reverse('home'))
     return redirect(referer)
 
