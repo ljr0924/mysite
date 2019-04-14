325a3945bd7b0d939b5b3da75d5e8dd8d9f998cb
Ljr <me824620662@vip.qq.com>
2019-04-14 23:16:55 +0800

    'user模块整理，点赞功能'

--- login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from login import views
-
-
-urlpatterns = [
-    path('login/', name='login'),
-    path('login/', name='login'),
-    path('login/', name='login'),
-]
